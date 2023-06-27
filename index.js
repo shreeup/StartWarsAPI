ce5ed73684d0e3e2f6f2b628dc241bce0ab342aa
shreeup <shree912@yahoo.com>
2023-06-27 12:28:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
+const ctnPlanets = document.getElementById("ctn-main");
+const planetsPrevious = document.getElementById("planets-previous");
+const planetsNext = document.getElementById("planets-next");
 
+// Variables Planets
+let URL_Planets = "https://swapi.dev/api/planets/?page=1";
+let nextPlanets;
+let previousPlanets;
+
+planetsPrevious.addEventListener("click", pagePreviousPlanets);
+planetsNext.addEventListener("click", pageNextPlanets);
+
+// Functions Planets
+async function fetchPlanets() {
+  document.querySelector('.overlay').classList.add('active');
+  let results = await fetch(URL_Planets);
+  const data = await results.json();
+  nextPlanets = data.next;
+  previousPlanets = data.previous;
+  let planets = data.results;
+  let outPut = ' ';
+  document.querySelector('.overlay').classList.remove('active');
+  planets.forEach(item => {
+    outPut += `<div class="card card-planet">
+                  <h2>${item.name}</h2>
+                  <h5>Climate: ${item.climate}</h5>
+                  <h5>Terrain: ${item.terrain}</h5>
+                  <h5>Population: ${item.population}</h5>
+               </div>`
+  })
+  ctnPlanets.innerHTML = outPut;
+}
+
+function pageNextPlanets() {
+  if(nextPlanets) {
+    URL_Planets = new URL(nextPlanets);
+  }
+  fetchPlanets()
+    .then(response => { 
+    console.log(`Success: Planets`);
+  })
+    .catch(error => { 
+    console.log(`error!`)
+    console.error(error) 
+  });
+}
